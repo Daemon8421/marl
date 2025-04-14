fd479c2b5e898e84184c5784113b3de8a31d91c9
Daemon8421 <kehang.zeng@gmail.com>
2025-04-14 14:20:18 +0800

    修改了bug，模型可以有效学习

--- maddpg/main.py
+++ maddpg/main.py
@@ -3,7 +3,10 @@ from maddpg import MADDPG
 
 if __name__ == '__main__':
     seed = 42
-    env = simple_adversary_v3.parallel_env(render_mode=None, continuous_actions=True)
+    env = simple_adversary_v3.parallel_env(render_mode=None,
+                                           continuous_actions=True)
     env.reset(seed)
     model = MADDPG(env)
     model.train(10000)
+    # model.train(1000)
+    model.save_checkpoint()
